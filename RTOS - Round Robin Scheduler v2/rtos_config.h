ab03262c6f751db14dab6f0df2c4dd3845ee294c
sg9924 <sg9924@users.noreply.github.com>
2025-08-30 17:41:08 +0530

    RR Scheduler v2 | Add Other files

---
+++ RTOS - Round Robin Scheduler v2/rtos_config.h
@@ -0,0 +1,9 @@
+#ifndef __RTOSCONFIG_H__
+#define __RTOSCONFIG_H__
+
+
+#define NO_OF_TASKS                                   3
+#define STACKSIZE                                     100
+
+
+#endif
