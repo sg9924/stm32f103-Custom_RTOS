bd46e956a88a569eab7d61e7b74dbc0c1999d2fa
sg9924 <sg9924@users.noreply.github.com>
2025-08-30 16:11:01 +0530

    RTOS Kernel Source | Initial
    1. TCB Array
    2. TCB Stack Array
    3. Pointer to current task
    4. Stack Init API Definition

---
+++ RTOS - Round Robin Scheduler/rtos_kernel.c
@@ -0,0 +1,47 @@
+#include<stdint.h>
+
+#include "rtos_kernel.h"
+#include "rtos_config.h"
+#include "rtos_task.h"
+#include "rtos_port.h"
+#include "stm32f103xx_serial.h"
+
+
+tcb_t TCBS[NO_OF_TASKS];  //declare an array of TCB's
+tcb_t *pcurrent;          //current pointer to a tcb
+
+
+int32_t TCBS_STACK[NO_OF_TASKS][STACKSIZE];     //array for stack for each Task
+
+
+//RTOS Kernel Stack Initialize
+//MSP is being used for both Tasks and Kernel
+void rtosKernel_StackInit(void)
+{
+    //initialize the stack pointer
+    for(uint8_t i=0; i<NO_OF_TASKS; i++)
+    {
+        TCBS[i].pstack = &(TCBS_STACK[i][STACKSIZE-16]);
+
+        //set T bit (bit 24) in xPSR to indicate Thumb state
+        TCBS_STACK[i][STACKSIZE-1] = (1<<24);
+
+        //initializing stack with dummy contents for other registers
+        //skipping PC ...
+        TCBS_STACK[i][STACKSIZE-3]  = 0xFFFFFFF9; //LR - Return to Thread mode and use MSP
+        TCBS_STACK[i][STACKSIZE-4]  = 0xAAAAAA12; //R12
+        TCBS_STACK[i][STACKSIZE-5]  = 0xAAAAAAA3; //R3
+        TCBS_STACK[i][STACKSIZE-6]  = 0xAAAAAAA2; //R2
+        TCBS_STACK[i][STACKSIZE-7]  = 0xAAAAAAA1; //R1
+        TCBS_STACK[i][STACKSIZE-8]  = 0xAAAAAAA0; //R0
+        
+        TCBS_STACK[i][STACKSIZE-9]  = 0xAAAAAA11; //R11
+        TCBS_STACK[i][STACKSIZE-10] = 0xAAAAAA10; //R10
+        TCBS_STACK[i][STACKSIZE-11] = 0xAAAAAAA9; //R9
+        TCBS_STACK[i][STACKSIZE-12] = 0xAAAAAAA8; //R8
+        TCBS_STACK[i][STACKSIZE-13] = 0xAAAAAAA7; //R7
+        TCBS_STACK[i][STACKSIZE-14] = 0xAAAAAAA6; //R6
+        TCBS_STACK[i][STACKSIZE-15] = 0xAAAAAAA5; //R5
+        TCBS_STACK[i][STACKSIZE-16] = 0xAAAAAAA4; //R4
+    }
+}
