cb6079b26fd059b119fbd26b7353b4774311c89e
sg9924 <sg9924@users.noreply.github.com>
2025-08-31 16:59:16 +0530

    RR Scheduler v2 | Demo Program
    Update the program for periodic task.

--- RTOS - Round Robin Scheduler v2/main.c
+++ RTOS - Round Robin Scheduler v2/main.c
@@ -3,7 +3,7 @@
 #include "stm32f103xx_init.h"
 #include "stm32f103xx_serial.h"
 
-#define TASK_QUANTA          500
+#define TASK_QUANTA_MS          500
 
 
 void task1(void)
@@ -23,12 +23,10 @@ void task2(void)
     }
 }
 
-void task3(void)
+void ptask3(void)
 {
-    while(1)
-    {
-        Serialprint("\r\nThis is Task 3 running...");
-    }
+    //Loop not included for Periodic Tasks
+    Serialprint("\r\nThis is Periodic Task 3 running...");
 }
 
 
@@ -40,10 +38,10 @@ int main(void)
     //Add the tasks
     taskAdd(&task1, "Task 1");
     taskAdd(&task2, "Task 2");
-    taskAdd(&task3, "Task 3");
+    taskAdd_Periodic(&ptask3, (5000/TASK_QUANTA_MS), "Periodic Task 3");
 
     //Launch Kernel
-    rtosKernel_Launch(TASK_QUANTA);
+    rtosKernel_Launch(TASK_QUANTA_MS);
 
     while(1)
     {
