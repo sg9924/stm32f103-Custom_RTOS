a58f19ffbe7fd1647b08eadeb200efe35750f70d
sg9924 <sg9924@users.noreply.github.com>
2025-09-12 19:50:04 +0530

    Update Fault Handler Lib
    Update Serialprint for the message types

--- Drivers/Src/stm32f103xx_fault_handlers.c
+++ Drivers/Src/stm32f103xx_fault_handlers.c
@@ -6,57 +6,57 @@
 //MMFSR
 void decode_MMFSR(uint8_t mmfsr, uint32_t mmfar, uint32_t cfsr)
 {
-    if(!mmfsr) {Serialprint("\r\n[DEBUG]: Empty MMFSR..."); return;}
-    Serialprint("\r\n[DEBUG]: MMFSR = %x", mmfsr);
-    if(cfsr & (1<<SCB_CFSR_IACC_VIOL))   Serialprint("\r\n[DEBUG]:   IACC_VIOL  - Instruction Access Violation");
-    if(cfsr & (1<<SCB_CFSR_DACC_VIOL))   Serialprint("\r\n[DEBUG]:   DACC_VIOL  - Data Access Violation");
-    if(cfsr & (1<<SCB_CFSR_MUNSTK_ERR))  Serialprint("\r\n[DEBUG]:   MUNSTK_ERR - Unstacking from Exception Exit Fault");
-    if(cfsr & (1<<SCB_CFSR_MSTK_ERR))    Serialprint("\r\n[DEBUG]:   MSTK_ERR   - Stacking of Exception Entry Fault");
-    if(cfsr & (1<<SCB_CFSR_MMAR_VALID))  Serialprint("\r\n[DEBUG]:   MMAR_VALID - MMFAR holds the Fault Address: %x", mmfar);
+    if(!mmfsr) {Serialprintln("Empty MMFSR...", DEBUG); return;}
+    Serialprintln("MMFSR = %x", DEBUG, mmfsr);
+    if(cfsr & (1<<SCB_CFSR_IACC_VIOL))   Serialprintln("  IACC_VIOL - Instruction Access Violation", DEBUG);
+    if(cfsr & (1<<SCB_CFSR_DACC_VIOL))   Serialprintln("  DACC_VIOL - Data Access Violation", DEBUG);
+    if(cfsr & (1<<SCB_CFSR_MUNSTK_ERR))  Serialprintln("  MUNSTK_ERR - Unstacking from Exception Exit Fault", DEBUG);
+    if(cfsr & (1<<SCB_CFSR_MSTK_ERR))    Serialprintln("  MSTK_ERR - Stacking of Exception Entry Fault", DEBUG);
+    if(cfsr & (1<<SCB_CFSR_MMAR_VALID))  Serialprintln("  MMAR_VALID - MMFAR holds the Fault Address: %x", DEBUG, mmfar);
 }
 
 //BFSR
 void decode_BFSR(uint8_t bfsr, uint32_t bfar, uint32_t cfsr)
 {
-    if(!bfsr) {Serialprint("\r\n[DEBUG]: Empty BFSR..."); return;}
-    Serialprint("\r\n[DEBUG]: BFSR = %x", bfsr);
-    if(cfsr & (1<<SCB_CFSR_IBUS_ERR))      Serialprint("\r\n[DEBUG]:   IBUS_ERR     - Instruction Bus Fault (Prefetch Abort)");
-    if(cfsr & (1<<SCB_CFSR_PRECIS_ERR))    Serialprint("\r\n[DEBUG]:   PRECIS_ERR   - Precise Data Bus Fault (Address in BFAR if Valid)");
-    if(cfsr & (1<<SCB_CFSR_IMPRECIS_ERR))  Serialprint("\r\n[DEBUG]:   IMPRECIS_ERR - Imprecise Data Bus Fault (Asynchronous)");
-    if(cfsr & (1<<SCB_CFSR_UNSTK_ERR))     Serialprint("\r\n[DEBUG]:   UNSTK_ERR    - Unstacking On Exception Exit Fault");
-    if(cfsr & (1<<SCB_CFSR_STK_ERR))       Serialprint("\r\n[DEBUG]:   STK_ERR      - Stacking on Exception Entry Fault");
-    if(cfsr & (1<<SCB_CFSR_BFAR_VALID))    Serialprint("\r\n[DEBUG]:   BFAR_VALID   - BFAR holds the Fault Address: %x", bfar);
+    if(!bfsr) {Serialprintln("Empty BFSR...", DEBUG); return;}
+    Serialprintln("BFSR = %x", DEBUG, bfsr);
+    if(cfsr & (1<<SCB_CFSR_IBUS_ERR))      Serialprintln("  IBUS_ERR - Instruction Bus Fault (Prefetch Abort)", DEBUG);
+    if(cfsr & (1<<SCB_CFSR_PRECIS_ERR))    Serialprintln("  PRECIS_ERR - Precise Data Bus Fault (Address in BFAR if Valid)", DEBUG);
+    if(cfsr & (1<<SCB_CFSR_IMPRECIS_ERR))  Serialprintln("  IMPRECIS_ERR - Imprecise Data Bus Fault (Asynchronous)", DEBUG);
+    if(cfsr & (1<<SCB_CFSR_UNSTK_ERR))     Serialprintln("  UNSTK_ERR - Unstacking On Exception Exit Fault", DEBUG);
+    if(cfsr & (1<<SCB_CFSR_STK_ERR))       Serialprintln("  STK_ERR - Stacking on Exception Entry Fault", DEBUG);
+    if(cfsr & (1<<SCB_CFSR_BFAR_VALID))    Serialprintln("  BFAR_VALID - BFAR holds the Fault Address: %x", DEBUG, bfar);
 }
 
 //UFSR
 void decode_UFSR(uint8_t ufsr, uint32_t cfsr)
 {
-    if(!ufsr) {Serialprint("\r\n[DEBUG]: Empty UFSR..."); return;}
-    Serialprint("\r\n[DEBUG]: UFSR = %x", ufsr);
-    if(cfsr & (1<<SCB_CFSR_UNDEFINSTR))   Serialprint("\r\n[DEBUG]:   UNDEFIN_STR - Undefined Instruction");
-    if(cfsr & (1<<SCB_CFSR_INV_STATE))    Serialprint("\r\n[DEBUG]:   INV_STATE   - Invalid State");
-    if(cfsr & (1<<SCB_CFSR_INVPC))        Serialprint("\r\n[DEBUG]:   INVPC       - Invalid PC Load (Illegal EXC_RETURN value, Invalid PSR)");
-    if(cfsr & (1<<SCB_CFSR_NOCP))         Serialprint("\r\n[DEBUG]:   NOCP        - No Coprocessor");
-    if(cfsr & (1<<SCB_CFSR_UNALIGNED))    Serialprint("\r\n[DEBUG]:   UNALIGNED   - Unaligned Access (If Trap has been enabled)");
-    if(cfsr & (1<<SCB_CFSR_DIVBYZERO))    Serialprint("\r\n[DEBUG]:   DIVBYZERO   - Divide by Zero (If Trap has been enabled)");
+    if(!ufsr) {Serialprintln("Empty UFSR...", DEBUG); return;}
+    Serialprintln("UFSR = %x", DEBUG, ufsr);
+    if(cfsr & (1<<SCB_CFSR_UNDEFINSTR))   Serialprintln("  UNDEFIN_STR - Undefined Instruction", DEBUG);
+    if(cfsr & (1<<SCB_CFSR_INV_STATE))    Serialprintln("  INV_STATE - Invalid State", DEBUG);
+    if(cfsr & (1<<SCB_CFSR_INVPC))        Serialprintln("  INVPC - Invalid PC Load (Illegal EXC_RETURN value, Invalid PSR)", DEBUG);
+    if(cfsr & (1<<SCB_CFSR_NOCP))         Serialprintln("  NOCP - No Coprocessor", DEBUG);
+    if(cfsr & (1<<SCB_CFSR_UNALIGNED))    Serialprintln("  UNALIGNED - Unaligned Access (If Trap has been enabled)", DEBUG);
+    if(cfsr & (1<<SCB_CFSR_DIVBYZERO))    Serialprintln("  DIVBYZERO - Divide by Zero (If Trap has been enabled)", DEBUG);
 }
 
 
 //HFSR
 void decode_HFSR(uint32_t hfsr)
 {
-    Serialprint("\r\n[DEBUG]: HFSR = %x", hfsr);
-    if(hfsr & (1<<SCB_HFSR_VECT_TBL)) Serialprint("\r\n[DEBUG]:   VECT_TBL - Fault on Vector Table Read");
-    if(hfsr & (1<<SCB_HFSR_FORCED))   Serialprint("\r\n[DEBUG]:   FORCED   - Escalated Fault - Check CFSR for the actual Fault");
-    if(hfsr & (1<<SCB_HFSR_DEBUG_VT)) Serialprint("\r\n[DEBUG]:   DEBUG_VT - Ddebug Event (Breakpoint, Watchpoint)");
+    Serialprintln("HFSR = %x", DEBUG, hfsr);
+    if(hfsr & (1<<SCB_HFSR_VECT_TBL)) Serialprintln("  VECT_TBL - Fault on Vector Table Read", DEBUG);
+    if(hfsr & (1<<SCB_HFSR_FORCED))   Serialprintln("  FORCED   - Escalated Fault - Check CFSR for the actual Fault", DEBUG);
+    if(hfsr & (1<<SCB_HFSR_DEBUG_VT)) Serialprintln("  DEBUG_VT - Ddebug Event (Breakpoint, Watchpoint)", DEBUG);
 }
 
 //CFSR
 void decode_CFSR(uint32_t cfsr, uint32_t mmfar, uint32_t bfar)
 {
-    if(!cfsr) {Serialprint("\r\n[DEBUG]: No Configurable Faults found"); return;}
+    if(!cfsr) {Serialprintln("CFSR = No Configurable Faults found", DEBUG); return;}
 
-    Serialprint("\r\n[DEBUG]: CFSR = %x", cfsr);
+    Serialprintln("CFSR = %x", DEBUG, cfsr);
 
     decode_MMFSR(MMFSR_READ(), cfsr, mmfar);
     decode_BFSR(BFSR_READ(), cfsr, bfar);
@@ -68,7 +68,7 @@ void decode_CFSR(uint32_t cfsr, uint32_t mmfar, uint32_t bfar)
 //HardFault Handler
 __attribute__((naked)) static void HardFault_Handler_C(uint32_t* pstack)
 {
-    Serialprint("\r\n[DEBUG]: --------------------------------------Hard Fault--------------------------------------");
+    Serialprintln("--------------------------------------Hard Fault Handler--------------------------------------", NONE);
 
     //uint32_t R0  = pstack[0];
     //uint32_t R1  = pstack[1];
@@ -84,18 +84,18 @@ __attribute__((naked)) static void HardFault_Handler_C(uint32_t* pstack)
     //uint32_t mmfar = SCB->MMFAR;
     //uint32_t bfar  = SCB->BFAR;
 
-    Serialprint("\r\n[DEBUG]: R0 = %x, R1 = %x, R2 = %x, R3 = %x", pstack[0],pstack[1],pstack[2],pstack[3]);
-    Serialprint("\r\n[DEBUG]: R12 = %x, LR = %x, PC = %x, PSR = %x", pstack[4],pstack[5],pstack[6],pstack[7]);
-    Serialprint("\r\n[DEBUG]: HFSR = %x", SCB->HFSR);
-    Serialprint("\r\n[DEBUG]: CFSR = %x", SCB->CFSR);
-    Serialprint("\r\n[DEBUG]: MMFAR = %x", SCB->MMFAR);
-    Serialprint("\r\n[DEBUG]: BFAR = %x\r\n", SCB->BFAR);
+    Serialprintln("R0 = %x, R1 = %x, R2 = %x, R3 = %x", DEBUG, pstack[0],pstack[1],pstack[2],pstack[3]);
+    Serialprintln("R12 = %x, LR = %x, PC = %x, PSR = %x", DEBUG, pstack[4],pstack[5],pstack[6],pstack[7]);
+    Serialprintln("HFSR = %x", DEBUG, SCB->HFSR);
+    Serialprintln("CFSR = %x", DEBUG, SCB->CFSR);
+    Serialprintln("MMFAR = %x", DEBUG, SCB->MMFAR);
+    Serialprintln("BFAR = %x\r\n", DEBUG, SCB->BFAR);
 
     decode_HFSR(SCB->HFSR);
     decode_CFSR(SCB->CFSR,SCB->MMFAR,SCB->BFAR);
 
-    Serialprint("\r\n[DEBUG]: Fault is at PC = %x (use addr2line with your elf to find the line at Source)", pstack[6]);
-    Serialprint("\r\n[DEBUG]: --------------------------------------------------------------------------------------");
+    Serialprintln("Fault is at PC = %x (use addr2line with your elf to find the line at Source)", DEBUG, pstack[6]);
+    Serialprintln("----------------------------------------------------------------------------------------------",  NONE);
 
     //Breakpoint
     __asm volatile ("BKPT #0");
