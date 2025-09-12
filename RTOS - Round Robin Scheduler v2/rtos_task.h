f07be9adbd23d29762ec91d4c194318440ca8fa0
sg9924 <sg9924@users.noreply.github.com>
2025-09-12 19:54:48 +0530

    RR Scheduler v2 | Tasks
    left out changes

--- RTOS - Round Robin Scheduler v2/rtos_task.h
+++ RTOS - Round Robin Scheduler v2/rtos_task.h
@@ -13,6 +13,7 @@
 typedef void(*ptask_t)(void);       //task function pointer
 
 //Task control Block
+//!order of structure members should not be changed!
 typedef struct tcb
 {
     int32_t*     pstack;                             //pointer to the stack
@@ -27,17 +28,18 @@ typedef struct tcb
 }tcb_t;
 
 void __task_count_init(void);
-ptask_t* getTaskList();
-tcb_t* getIdleTask_TCB();
-uint8_t getTaskCount();
-
 void taskAdd(ptask_t func_ptr, char* task_desc);
 void taskAdd_Periodic(ptask_t func_ptr, uint32_t period, char* task_desc);
-void taskAdd_Idle(void);
 void taskDelay(uint32_t tick);
 void taskIdle(void);
 void taskUnblock(void);
 void taskYield(void);
 
+void taskAdd_Idle();
+
+ptask_t getTaskFunc(uint8_t task_num);
+tcb_t* getIdleTask_TCB();
+ptask_t* getTaskList();
+uint8_t getTaskCount();
 
 #endif
