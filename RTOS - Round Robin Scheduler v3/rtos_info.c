13443dde751d1368dd9a2f61955bba4a71d4e043
sg9924 <sg9924@users.noreply.github.com>
2025-09-12 19:58:56 +0530

    RR Scheduler v3 | RTOS Info
    Update for changes in Serialprint

--- RTOS - Round Robin Scheduler v3/rtos_info.c
+++ RTOS - Round Robin Scheduler v3/rtos_info.c
@@ -10,15 +10,14 @@ extern tcb_t *pcurrent;              //current pointer to a tcb
 void rtosInfo_Tasks(void)
 {
     tcb_t* temp = pcurrent;
-    Serialprint("\r\n--------------------------------RTOS Tasks Info--------------------------------");
+    Serialprintln("--------------------------------RTOS Tasks Info--------------------------------", INFO);
     for(uint8_t i=0; i<NO_OF_TASKS+1; i++)
     {
-        Serialprint("\r\nTask ID: %d | Task Address: %x | Task Description: %s", temp->task_id, temp, temp->task_desc);
+        Serialprintln("Task ID: %d | Task Address: %x | Task Description: %s", INFO, temp->task_id, temp, temp->task_desc);
         temp = temp->pnext;
     }
-    Serialprint("\r\n-------------------------------------------------------------------------------");
-    Serialprint("\r\n");
+    Serialprintln("-------------------------------------------------------------------------------", INFO);
 
-    Serialprint("\r\nStarting the Scheduler now...\r\n");
+    Serialprintln("Starting the Scheduler now...\r\n", INFO);
     tim_delay_ms(5000);
 }
