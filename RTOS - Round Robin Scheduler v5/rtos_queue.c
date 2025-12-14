a78f3402b64be6a5be6190c42f40e059f8a1704b
sg9924 <sg9924@users.noreply.github.com>
2025-12-14 15:14:11 +0530

    RR Scheduler v5 | Queue| Bug Fixes and Improvements
    - Modify: ready_queue_remove, blocked_queue_remove,

--- RTOS - Round Robin Scheduler v5/rtos_queue.c
+++ RTOS - Round Robin Scheduler v5/rtos_queue.c
@@ -11,7 +11,6 @@ extern tcb_t TCBS[NO_OF_TASKS+1];                 //an array of TCB's
 
 void ready_queue_add(tcb_t* task)
 {
-
     //set state as ready
     task->task_state = TASK_STATE_READY;
     task->pnext      = NULL;
@@ -132,16 +131,26 @@ uint8_t ready_queue_remove(tcb_t* task, uint8_t state)
         //get the starting task of the specific priority
         tcb_t* i = ready_queue[0];
 
-        //iterate till the end of the linked list
-        while(i->pnext != NULL)
+        //starting task of queue matches
+        if(i == task)
         {
-            if(i->pnext == task)
+            i->task_state = state;
+            blocked_queue[0] = i->pnext;
+        }
+        //first task didn't match
+        else
+        {   
+            //iterate
+            while(i->pnext != NULL)
             {
-                i->pnext = (i->pnext)->pnext;
-                i->pnext->task_state = state;
-                return 1;
+                if(i->pnext == task)
+                {
+                    (i->pnext)->task_state = state;
+                    i = (i->pnext)->pnext;
+                    return 1;
+                }
+                i = i->pnext;
             }
-            i = i->pnext;
         }
     }
     return 0;
@@ -155,16 +164,28 @@ uint8_t blocked_queue_remove(tcb_t* task, uint8_t state)
         //get the starting task of the specific priority
         tcb_t* i = blocked_queue[0];
 
-        //iterate till the end of the linked list
-        while(i->pnext != NULL)
+        //starting task of queue matches
+        if(i == task)
         {
-            if(i->pnext == task)
+            i->task_state = state;
+            i->block_tick = 0;
+            blocked_queue[0] = i->pnext;
+        }
+        //first task didn't match
+        else
+        {   
+            //iterate
+            while(i->pnext != NULL)
             {
-                i->pnext = (i->pnext)->pnext;
-                i->pnext->task_state = state;
-                return 1;
+                if(i->pnext == task)
+                {
+                    (i->pnext)->task_state = state;
+                    (i->pnext)->block_tick = 0;
+                    i = (i->pnext)->pnext;
+                    return 1;
+                }
+                i = i->pnext;
             }
-            i = i->pnext;
         }
     }
     return 0;
