0d3c7729c4e424010589b5cf78f0c51c3717acde
sg9924 <sg9924@users.noreply.github.com>
2025-12-14 16:28:34 +0530

    RR Scheduler v6 | Sample Program

--- RTOS - Round Robin Scheduler v6/main.c
+++ RTOS - Round Robin Scheduler v6/main.c
@@ -7,6 +7,7 @@
 
 //int8_t s1, s2;
 
+uint8_t flag = 1;
 tcb_t* tcb_list[NO_OF_TASKS+1];
 
 
@@ -45,19 +46,32 @@ task code
 
 void task1(void)
 {
+    uint32_t noti_value;
     while(1)
     {
-        Serialprintln("[Tick: %x] [ID: %d] This is Task 1 running...", INFO, Systick_get_tick(), tcb_list[1]->task_id);
-        //taskDelay(5);
+        Serialprintln("[Tick: %x] [Task ID: %d] This is Task 1 running...", INFO, Systick_get_tick(), tcb_list[1]->task_id);
+        
+        if(flag)
+        {
+            if(taskNotify_Wait(0, 0, &noti_value, 10)) //wait for noti from task 2
+                Serialprintln("[Tick: %x] [Task ID: %d] Notification Received from Task 2! with Value %d", INFO, Systick_get_tick(), tcb_list[1]->task_id, noti_value);
+        }
     }
 }
 
 void task2(void)
 {
     while(1)
-    {
-        Serialprintln("[Tick: %x] [ID: %d] This is Task 2 running...", INFO, Systick_get_tick(), tcb_list[2]->task_id); 
-    }
+   {
+        Serialprintln("[Tick: %x] [Task ID: %d] This is Task 2 running...", INFO, Systick_get_tick(), tcb_list[2]->task_id);
+
+        if(flag)
+        {
+            taskNotify_Send(tcb_list[1], 1, TASK_NOTIFY_ACTION_INC); //send noti to task 1
+            Serialprintln("[Tick: %x] [Task ID: %d] Notification Sent to Task 1!", INFO, Systick_get_tick(), tcb_list[2]->task_id);
+        }
+        flag = 0;
+   }
 }
 
 
