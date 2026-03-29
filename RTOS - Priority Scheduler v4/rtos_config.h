b408d72ea5228866dc3ce8341e4d724e0816f28e
sg9924 <sg9924@users.noreply.github.com>
2026-03-29 15:35:59 +0530

    Priority Scheduler v4 | Config | Modify

--- RTOS - Priority Scheduler v4/rtos_config.h
+++ RTOS - Priority Scheduler v4/rtos_config.h
@@ -8,19 +8,19 @@
 #define NO_OF_TASKS                                   3
 #define STACKSIZE                                     200
 #define TASK_DESC_LENGTH                              100
-#define TASK_MAX_PRIORITY                             4
+#define MAX_NO_OF_PRIORITY                            4
 #define RTOS_SCHEDULER_START_DELAY                    3000
 #define SCHEDULER                                     SCHEDULER_PRIORITY
 
-//Callbacks
-#define IDLE_TASK_CALLBACK                            1
+#define SCHEDULER_PRIORITY                            (1<<2)
 
 
 //Configurations
 //0 - disabled
 //1 - enabled
 #define INFO_PRINT                                    1
+#define IDLE_TASK_PRINT                               0
+#define SYSTICK_LED_TOGGLE                            1
 
-#define SCHEDULER_PRIORITY                            (1<<2)
 
 #endif
