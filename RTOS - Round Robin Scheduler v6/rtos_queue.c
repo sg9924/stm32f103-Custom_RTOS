58fec0968a5d38acc280220422183df0544ec94b
sg9924 <sg9924@users.noreply.github.com>
2026-07-27 08:42:43 +0530

    RR Scheduler v6 | Cleanup

--- RTOS - Round Robin Scheduler v6/rtos_queue.c
+++
@@ -1,2 +0,0 @@
-#include"rtos_queue.h"
-#include"rtos_port.h"
--- RTOS - Round Robin Scheduler v6/rtos_queue.h
+++
@@ -1,9 +0,0 @@
-#ifndef __RTOSQUEUE_H__
-#define __RTOSQUEUE_H__
-#include <stdint.h>
-
-#include "rtos_task.h"
-#include "rtos_config.h"
-
-
-#endif
